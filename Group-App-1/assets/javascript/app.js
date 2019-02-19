61f735e31dd6da6fb3cb857f82343bb6f711f8f0
William Baires <baireswilliame@gmail.com>
2019-02-19 11:08:56 -0800

    first commit

---
+++ Group-App-1/assets/javascript/app.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsbGlhbWJhaXJlcyIsImEiOiJjanJzcTl3ZHEwcWNsNDNsd3c0b2hiajFyIn0.uuRv7diqGI-Kytr9dYR8Tw';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
+
+
+
